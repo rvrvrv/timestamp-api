605621f1540c241040cb1b16d25e806e42e6d917
Rich <ibanez19@sbcglobal.net>
2017-01-17 17:30:28 +0000

    Modified package.json to include Start command

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
 var express = require('express');
-var app = express()
+var strftime = require('strftime'); //Date-formatting module
+var app = express(); //Simple server setup
+var port = process.env.PORT || 8080; //Let Heroku set the port
 
+//Landing page with instructions
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.sendfile(__dirname + '/index.html');
+});
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
-})
+
+//Actual Timestamp API functionality
+app.get('/:timeOrDate', function(req, res) {
+  //Convert user entry to a number
+  var userEntry = Number(req.params.timeOrDate);
+  
+  //If user entry isn't a number, parse the date out of it
+  if (isNaN(userEntry)) var theTime = Date.parse(req.params.timeOrDate);
+  /*If user entry is a number, then it's a unix timestamp.
+  Value is multiplied by 1000 for proper JS millisecond (ms) value*/
+  else theTime = new Date(userEntry*1000);
+  
+  //If entry is invalid (couldn't be converted), return both null
+  if (isNaN(theTime)) res.send({'unix':null,'natural':null});
+  //Otherwise, return the correctly formatted values
+  else res.send({
+    'unix':(theTime/1000), //Divided by 1000 for correct unix output
+    'natural':strftime('%B %d, %Y', new Date(theTime))
+  });
+});
+
+
+app.listen(port, function () {
+  console.log('This app is running on port ' + port);
+});
+
