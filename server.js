8efe5b093a8792751471d855db9a643d8a4d9a58
Rich <ibanez19@sbcglobal.net>
2018-09-26 16:35:22 +0000

    Use ES6

--- server.js
+++ server.js
@@ -1,36 +1,32 @@
-var express = require('express');
-var strftime = require('strftime'); //Date-formatting module
-var app = express(); //Simple server setup
-var port = process.env.PORT || 8080; //Let Heroku set the port
+const express = require('express');
+const strftime = require('strftime'); // Date-formatting module
+const app = express(); // Simple server setup
+const port = process.env.PORT || 8080; // Let Heroku set the port
 
-//Landing page with instructions
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+// Landing page with instructions
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
 });
 
-
-//Actual Timestamp API functionality
-app.get('/:timeOrDate', function(req, res) {
-  //Convert user entry to a number
-  var userEntry = Number(req.params.timeOrDate);
+// Actual Timestamp API functionality
+app.get('/:timeOrDate', (req, res) => {
+  // Convert user entry to a number
+  const userEntry = Number(req.params.timeOrDate);
   
-  //If user entry isn't a number, parse the date out of it
-  if (isNaN(userEntry)) var theTime = Date.parse(req.params.timeOrDate);
-  /*If user entry is a number, then it's a unix timestamp.
-  Value is multiplied by 1000 for proper JS millisecond (ms) value*/
-  else theTime = new Date(userEntry*1000);
+  // Handle userEntry
+  const theTime = isNaN(userEntry)
+    ? Date.parse(req.params.timeOrDate) // Not a number? Parse date
+    : new Date(userEntry * 1000)
+  /* ^^ If user entry is a number, then it's a unix timestamp.
+  Multiple value by 1000 for proper ms value */
   
-  //If entry is invalid (couldn't be converted), return both null
-  if (isNaN(theTime)) res.send({'unix':null,'natural':null});
-  //Otherwise, return the correctly formatted values
+  // If entry is invalid (couldn't be converted), return both null
+  if (isNaN(theTime)) res.send({ 'unix': null, 'natural': null });
+  // Otherwise, return the correctly formatted values
   else res.send({
-    'unix':(theTime/1000), //Divided by 1000 for correct unix output
-    'natural':strftime('%B %d, %Y', new Date(theTime))
+    'unix': (theTime / 1000), // Divided by 1000 for correct unix output
+    'natural': strftime('%B %d, %Y', new Date(theTime))
   });
 });
 
-
-app.listen(port, function () {
-  console.log('This app is running on port ' + port);
-});
-
+app.listen(port, () => console.log(`This app is running on port ${port}`));
